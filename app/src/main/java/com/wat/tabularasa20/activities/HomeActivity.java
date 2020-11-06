6c4b953ce0b354827407ab441af269fa772f74c8
Michaltyk2 <50484557+Michaltyk2@users.noreply.github.com>
2020-11-06 19:36:41 +0100

    Update

---
+++ app/src/main/java/com/wat/tabularasa20/activities/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.wat.tabularasa20.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.wat.tabularasa20.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        TextView homeTV = findViewById(R.id.homeTextView);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String result = intent.getStringExtra("result");
+
+        // Tak, to nie ma sensu, to tylko test JSON
+        JsonObject jsonObject = JsonParser.parseString("{\"result\": " + result + "}").getAsJsonObject();
+
+        homeTV.setText("Witaj " + name + "\nserwer odpowiedział: " +  jsonObject.get("result").getAsString());
+    }
+}
