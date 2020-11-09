5f5ec018172700ff381a0cc9d4cb61b671934904
Michaltyk2 <50484557+Michaltyk2@users.noreply.github.com>
2020-11-10 00:04:58 +0100

    Kolejna zmiana

--- app/src/main/java/com/wat/tabularasa20/utilities/Downloader.java
+++ app/src/main/java/com/wat/tabularasa20/utilities/Downloader.java
@@ -28,7 +28,7 @@ public class Downloader extends AsyncTask<Object, Void, String> {
         try {
             StringBuilder sb = new StringBuilder();
             URL url = new URL((String) params[0]);
-            //*
+            /*
             String passwdstring = "USERNAME:PASSWORD";
             String encoding = Base64.encodeToString(passwdstring.getBytes(), Base64.DEFAULT);
             URLConnection urlConnection = url.openConnection();
@@ -36,7 +36,7 @@ public class Downloader extends AsyncTask<Object, Void, String> {
             urlConnection.setRequestProperty("Authorization", "Authorization: AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request, SignedHeaders=host;range;x-amz-date, Signature=fe5f80f77d5fa3beca038a248ff027d0445342fe2855ddc963176630326f1024");
             InputStream is = urlConnection.getInputStream();
             //*/
-            //InputStream is = url.openStream();
+            InputStream is = url.openStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(is));
             String inputLine;
             while ((inputLine = in.readLine()) != null)
