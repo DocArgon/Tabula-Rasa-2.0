5d53b0ebfcc12055d85ec73fbdbfef7549a952f2
piotr-zaborski <piotrzabor2@gmail.com>
2020-11-09 23:41:07 +0100

    Dodanie 3 ekranów: rejestracji, szukania ludzi, listy ulubionych książek. Dodanie przycisków do ekranu zarządzania kontem

--- app/src/main/java/com/wat/tabularasa20/MainActivity.java
+++ app/src/main/java/com/wat/tabularasa20/MainActivity.java
@@ -1,95 +1,95 @@
 package com.wat.tabularasa20;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-import com.google.android.material.snackbar.Snackbar;
+
+import com.wat.tabularasa20.activities.HomeActivity;
 import com.wat.tabularasa20.data.Constants;
 import com.wat.tabularasa20.utilities.Downloader;
-import com.wat.tabularasa20.utilities.Network;
-import com.wat.tabularasa20.utilities.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET},1234);
-        }
 
-        final ImageView iv = findViewById(R.id.mainApplicationLogo);
-        final EditText textName = findViewById(R.id.mainEditTextName);
-        final EditText textPass = findViewById(R.id.mainEditTextPassword);
-        final Button buttonLogin = findViewById(R.id.mainButtonLogin);
+		int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+		if (permission != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET},1234);
+		}
 
-        // Akcja downloadera
-        Downloader downloader = new Downloader();
-        downloader.setOnResultListener(result -> {
-            // TODO sprawdzenie czy odpowiedź zezwala na dostęp
+		// TODO sprawdzić połączenie internetowe
 
-            Preferences.saveCredentials(MainActivity.this,
-                    new Preferences.LoginCredentials(textName.getText(), textPass.getText()));
+		final ImageView iv = findViewById(R.id.profilePicture);
+		final EditText name = findViewById(R.id.addBookTitle);
+		final EditText pass = findViewById(R.id.mainEditTextPassword);
+		final Button login = findViewById(R.id.addBookSendData);
 
-            Toast.makeText(MainActivity.this, "is null " + (result == null), Toast.LENGTH_SHORT).show();
-            Toast.makeText(MainActivity.this, "\"" + result + "\"", Toast.LENGTH_LONG).show();
-            /*
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-            intent.putExtra("name", textName.getText().toString());
-            intent.putExtra("result", result);
-            startActivity(intent);
-            finish(); // Klasy Downloader nie wykorzystywać 2 raz - wymaga reinicjalizacji dlatego kończę aktywność
-            //*/
-        });
+		// Akcja downloadera
+		Downloader downloader = new Downloader();
+		downloader.setOnResultListener(new Downloader.DownloadActions() {
+			@Override
+			public void getResult(String result) {
+				Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+				intent.putExtra("name", name.getText().toString());
+				intent.putExtra("result", result);
+				startActivity(intent);
+				finish(); // Klasy Downloader nie wykorzystywać 2 raz - wymaga reinicjalizacji dlatego kończę aktywność
+			}
+		});
 
-        // Akcja przycisku
-        buttonLogin.setOnClickListener(v -> {
-            String strname = textName.getText().toString();
-            String strpass = textPass.getText().toString();
+		// Akcja przycisku
+		login.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String strname = name.getText().toString();
+				String strpass = pass.getText().toString();
 
-            if (!Network.isDeviceConnected(MainActivity.this)) {
-                Snackbar.make(v, getString(R.string.network_not_conn), Snackbar.LENGTH_LONG).show();
-                return;
-            }
+				// Usunę to, tak na wszelki
+                /*
+                if (strpass.equals("2137")) {
+                    iv.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.flan2));
+                    new CountDownTimer(100, 50) {
+                        public void onTick(long millisUntilFinished) {}
+                        public void onFinish() {
+                            iv.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), android.R.drawable.btn_star_big_off));
+                        }
+                    }.start();
+                }
+                //*/
 
-            // TODO przerobić zapytanie
-            if (!strname.isEmpty() && !strpass.isEmpty()) {
-                String strurl = Constants.WELCOME_URL + strname.substring(strname.length() - 1);
-                downloader.execute("http://cfbhkldsnmvrtwzx.neverssl.com/online");
-                //downloader.execute("https://6ztwzizmp2.execute-api.eu-west-1.amazonaws.com/v1");
-                //downloader.execute(strurl);
-            } else {
-                Snackbar.make(v, getString(R.string.login_fields_empty), Snackbar.LENGTH_LONG).show();
-            }
-        });
+				if (!strname.isEmpty() && !strpass.isEmpty()) {
+					String strurl = Constants.WELCOME_URL + strname.substring(name.length() - 1);
+					downloader.execute(strurl);
+				} else {
+					// TODO powiadomienie o wymogu danych logowania
+				}
+			}
+		});
+	}
 
-        // Jeśli zapamiętano dane logowania to zaloguj automatycznie
-        Preferences.LoginCredentials credentials = Preferences.readCredential(this);
-        if (credentials != null) {
-            textName.setText(credentials.login);
-            textPass.setText(credentials.password);
-            buttonLogin.performClick();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 1234) {
-            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, getString(R.string.permission_not_granted), Toast.LENGTH_LONG).show();
-                //finish();
-            }
-        }
-    }
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case 1234: {
+				if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+					Toast.makeText(this, "Nie przyznano uprawnień", Toast.LENGTH_LONG).show();
+					//finish();
+				}
+			}
+		}
+	}
 }
