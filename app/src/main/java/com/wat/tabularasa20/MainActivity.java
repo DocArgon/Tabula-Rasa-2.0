3a870076ae18b42b327170675be2ba76de845f65
piotr-zaborski <piotrzabor2@gmail.com>
2020-11-09 20:41:40 +0100

    Dodanie 2 ekranu logowania i ekranu konta

--- app/src/main/java/com/wat/tabularasa20/MainActivity.java
+++ app/src/main/java/com/wat/tabularasa20/MainActivity.java
@@ -3,61 +3,66 @@ package com.wat.tabularasa20;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.wat.tabularasa20.activities.HomeActivity;
 import com.wat.tabularasa20.data.Constants;
 import com.wat.tabularasa20.utilities.Downloader;
-import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET},1234);
-        }
 
-        // TODO sprawdzić połączenie internetowe
+		int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+		if (permission != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET},1234);
+		}
 
-        final ImageView iv = findViewById(R.id.mainImageView);
-        final EditText name = findViewById(R.id.mainEditTextName);
-        final EditText pass = findViewById(R.id.mainEditTextPassword);
-        final Button login = findViewById(R.id.mainButtonLogin);
+		// TODO sprawdzić połączenie internetowe
 
-        // Akcja downloadera
-        Downloader downloader = new Downloader();
-        downloader.setOnResultListener(new Downloader.DownloadActions() {
-            @Override
-            public void getResult(String result) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                intent.putExtra("name", name.getText().toString());
-                intent.putExtra("result", result);
-                startActivity(intent);
-                finish(); // Klasy Downloader nie wykorzystywać 2 raz - wymaga reinicjalizacji dlatego kończę aktywność
-            }
-        });
+		final ImageView iv = findViewById(R.id.profilePicture);
+		final EditText name = findViewById(R.id.mainEditTextName);
+		final EditText pass = findViewById(R.id.mainEditTextPassword);
+		final Button login = findViewById(R.id.mainButtonLogin);
 
-        // Akcja przycisku
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String strname = name.getText().toString();
-                String strpass = pass.getText().toString();
+		// Akcja downloadera
+		Downloader downloader = new Downloader();
+		downloader.setOnResultListener(new Downloader.DownloadActions() {
+			@Override
+			public void getResult(String result) {
+				Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+				intent.putExtra("name", name.getText().toString());
+				intent.putExtra("result", result);
+				startActivity(intent);
+				finish(); // Klasy Downloader nie wykorzystywać 2 raz - wymaga reinicjalizacji dlatego kończę aktywność
+			}
+		});
 
-                // Usunę to, tak na wszelki
+		// Akcja przycisku
+		login.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String strname = name.getText().toString();
+				String strpass = pass.getText().toString();
+
+				// Usunę to, tak na wszelki
                 /*
                 if (strpass.equals("2137")) {
                     iv.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.flan2));
@@ -70,25 +75,25 @@ public class MainActivity extends AppCompatActivity {
                 }
                 //*/
 
-                if (!strname.isEmpty() && !strpass.isEmpty()) {
-                    String strurl = Constants.WELCOME_URL + strname.substring(strname.length() - 1);
-                    downloader.execute(strurl);
-                } else {
-                    // TODO powiadomienie o wymogu danych logowania
-                }
-            }
-        });
-    }
+				if (!strname.isEmpty() && !strpass.isEmpty()) {
+					String strurl = Constants.WELCOME_URL + strname.substringname.length() - 1);
+					downloader.execute(strurl);
+				} else {
+					// TODO powiadomienie o wymogu danych logowania
+				}
+			}
+		});
+	}
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1234: {
-                if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Nie przyznano uprawnień", Toast.LENGTH_LONG).show();
-                    //finish();
-                }
-            }
-        }
-    }
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case 1234: {
+				if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+					Toast.makeText(this, "Nie przyznano uprawnień", Toast.LENGTH(str_LONG).show();
+					//finish();
+				}
+			}
+		}
+	}
 }
